24f18b9bfd5c6d522268641b28610f9a939cfefa
NarenNades <344941885@tdsb.ca>
2026-03-12 14:13:09 +0000

    Done!

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,38 @@
+/**
+ * File: More Functions
+ * Author: Naren
+ * Date Created: Mar 11, 2026
+ * Date Last Modified: Mar 12, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+	public static Boolean isEven(int num1) {
+		Boolean isEven = (num1 % 2 == 0);
+		return isEven;
+	}
+
+	public static Boolean isOdd(int num1) {
+		Boolean isOdd = (num1 % 2 != 0);
+		return isOdd;
+	}
+
+	public static Boolean isPositive(int num1) {
+		Boolean isPositive = (num1 > 0);
+		return isPositive;
+	}
+
+	public static Boolean isNegative(int num1) {
+		Boolean isPositive = (num1 < 0);
+		return isPositive;
+	}
+
+	public static int combinedLength(String string1, String string2) {
+		int combinedLength = string1.length() + string2.length();
+		return combinedLength;
+	}
 }
--- src/test/java/PracticeProblemTest.java
+++ src/test/java/PracticeProblemTest.java
@@ -1,7 +1,8 @@
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.*;
-import java.io.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
